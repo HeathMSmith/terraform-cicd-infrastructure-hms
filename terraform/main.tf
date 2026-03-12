c4c758dfe5ca17393ee1a0653d187af0ac8388ed
Heath Smith <heathsmithaws2025@gmail.com>
2026-03-11 21:13:21 -0400

    fix tfsec S3 security findings

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,3 +28,22 @@ resource "aws_s3_bucket_public_access_block" "example_block" {
   block_public_policy     = true
   restrict_public_buckets = true
 }
+
+resource "aws_s3_bucket_versioning" "example_versioning" {
+  bucket = aws_s3_bucket.example.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example_encryption" {
+  bucket = aws_s3_bucket.example.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+#tfsec:ignore:aws-s3-encryption-customer-key
