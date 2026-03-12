9fbc7fedbe9e3edfe160b01ce9e2a5a916d02d36
Heath Smith <heathsmithaws2025@gmail.com>
2026-03-11 21:03:49 -0400

    fix tfsec S3 public access block

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,3 +20,11 @@ resource "aws_s3_bucket" "example" {
     Environment = "dev"
   }
 }
+resource "aws_s3_bucket_public_access_block" "example_block" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+}
